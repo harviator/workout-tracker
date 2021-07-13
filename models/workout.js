0db68b6d740a9bde3819f191200cdcd0bce08bb8
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-13 19:56:39 -0400

    finished the workout model

--- models/workout.js
+++ models/workout.js
@@ -17,34 +17,32 @@ const workoutSchema = new Schema({
             type: {
                 type: String,
                 trim: true,
-                required: "Enter the name of the exercise"
+                required: "Enter the name of type of exercise (either cardio or strength)"
             },
             weight: {
-                type: String,
-                trim: true,
-                required: "Enter the name of the exercise"
+                type: Number,
+                required: "Enter the weight of the iron you pumped"
             },
             sets: {
-                type: String,
-                trim: true,
-                required: "Enter the name of the exercise"
+                type: Number,
+                required: "Enter how many sets you did to look like Hulk"
             },
             reps: {
-                type: String,
-                trim: true,
-                required: "Enter the name of the exercise"
+                type: Number,
+                required: "Enter the number of reps you did"
             },
             duration: {
-                type: String,
-                trim: true,
-                required: "Enter the name of the exercise"
+                type: Number,
+                required: "Enter the duration of your workout"
             },
             distance: {
-                type: String,
-                trim: true,
-                required: "Enter the name of the exercise"
+                type: Number,
+                required: "Enter the distance.  It could be feet, meters, miles, or kilometers we don't judge"
             },
         }
     ]
+});
 
-}) 
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
