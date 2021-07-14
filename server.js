01f9c08315342bd10f1475a37353cf56871b0def
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-14 16:51:58 -0400

    Added some get routes to the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const db = require("./models");
+const path =require('path')
 const PORT = process.env.PORT || 3000
 
 const app = express();
@@ -18,8 +19,51 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useUnifiedTopology: true
 });
 
-// routes
-// app.use(require("./public/api.js"));
+
+//home routes
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'))
+
+ 
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+
+ 
+});
+
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'))
+
+ 
+});
+
+//api routes
+app.get('api/workouts',(req,res)=>{
+db.Workout
+
+db.Workout.aggregate( [
+  {
+    $addFields: {
+      totalDuration: { $sum: "$exercises.duration" } ,
+    }
+  },
+  
+] ).then(dbWorkouts=>{
+  res.json(dbWorkouts)
+})
+
+
+})
+ // db.Workout.find({})
+  //   .then(dbWorkout => {
+  //     res.json(dbWorkout);
+  //   })
+  //   .catch(err => {
+  //     res.json(err);
+  //   });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
